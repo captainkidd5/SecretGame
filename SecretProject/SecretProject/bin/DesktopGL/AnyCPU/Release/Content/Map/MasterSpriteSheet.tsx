13f0f7f5f668b3d3202bf1ce2409d308d80031b2
Tucker Higgins <thiggin6@lion.lmu.edu>
2019-03-31 19:51:43 -0700

    xml built succesfully, I just dont know where it went...

---
+++ SecretProject/SecretProject/bin/DesktopGL/AnyCPU/Release/Content/Map/MasterSpriteSheet.tsx
@@ -0,0 +1,104 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="MasterSpriteSheet" tilewidth="16" tileheight="16" tilecount="10000" columns="100">
+ <image source="../../../Map/MasterSpriteSheet.png" width="1600" height="1600"/>
+ <tile id="6065">
+  <properties>
+   <property name="diggable" value="dirt"/>
+  </properties>
+ </tile>
+ <tile id="6066">
+  <properties>
+   <property name="diggable" value="dirt"/>
+  </properties>
+ </tile>
+ <tile id="6067">
+  <properties>
+   <property name="diggable" value="dirt"/>
+  </properties>
+ </tile>
+ <tile id="6068">
+  <properties>
+   <property name="diggable" value="dirt"/>
+  </properties>
+ </tile>
+ <tile id="6165">
+  <properties>
+   <property name="diggable" value="dirt"/>
+  </properties>
+ </tile>
+ <tile id="6166">
+  <properties>
+   <property name="diggable" value="dirt"/>
+  </properties>
+ </tile>
+ <tile id="6167">
+  <properties>
+   <property name="diggable" value="dirt"/>
+  </properties>
+ </tile>
+ <tile id="6168">
+  <properties>
+   <property name="diggable" value="dirt"/>
+  </properties>
+ </tile>
+ <tile id="6265">
+  <properties>
+   <property name="diggable" value="dirt"/>
+  </properties>
+ </tile>
+ <tile id="6266">
+  <properties>
+   <property name="diggable" value="dirt"/>
+  </properties>
+ </tile>
+ <tile id="6267">
+  <properties>
+   <property name="diggable" value="dirt"/>
+  </properties>
+ </tile>
+ <tile id="6268">
+  <properties>
+   <property name="diggable" value="dirt"/>
+  </properties>
+ </tile>
+ <tile id="6365">
+  <properties>
+   <property name="diggable" value="dirt"/>
+  </properties>
+ </tile>
+ <tile id="6366">
+  <properties>
+   <property name="diggable" value="dirt"/>
+  </properties>
+ </tile>
+ <tile id="6367">
+  <properties>
+   <property name="diggable" value="dirt"/>
+  </properties>
+ </tile>
+ <tile id="6368">
+  <properties>
+   <property name="diggable" value="dirt"/>
+  </properties>
+ </tile>
+ <tile id="6474">
+  <properties>
+   <property name="Animated" value="6"/>
+   <property name="Speed" value=".15"/>
+   <property name="grass" value="true"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="1" x="1" y="1" width="13" height="13"/>
+  </objectgroup>
+ </tile>
+ <tile id="6674">
+  <properties>
+   <property name="Animated" value="5"/>
+   <property name="Speed" value=".15"/>
+   <property name="stone" value=""/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="1" x="1" y="2" width="13" height="12"/>
+  </objectgroup>
+ </tile>
+</tileset>
