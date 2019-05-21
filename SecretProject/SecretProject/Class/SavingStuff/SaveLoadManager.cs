204cbcd086cc1e1a14af7f54da9b7b921fafd6c7
Tucker Higgins <thiggin6@lion.lmu.edu>
2019-05-20 17:42:16 -0700

    working on binary serialization

--- SecretProject/SecretProject/Class/SavingStuff/SaveLoadManager.cs
+++ SecretProject/SecretProject/Class/SavingStuff/SaveLoadManager.cs
@@ -8,6 +8,7 @@ using System.Xml.Serialization;
 using SecretProject.Class.Playable;
 using Microsoft.Xna.Framework;
 using SecretProject.Class.ItemStuff;
+using SecretProject.Class.Universal;
 
 namespace SecretProject.Class.SavingStuff
 {
@@ -23,27 +24,44 @@ namespace SecretProject.Class.SavingStuff
 
         public void Save()
         {
-            mySave = new SaveData();
-            XmlSerializer serializer = new XmlSerializer(typeof(SaveData));
-            using (FileStream tw = new FileStream(@"Content/SaveFiles/GameTestSave3.xml", FileMode.Create))
-            {
-                serializer.Serialize(tw, mySave);
-                tw.Close();
-            }
+            //mySave = new SaveData();
+            //XmlSerializer serializer = new XmlSerializer(typeof(SaveData));
+            //using (FileStream tw = new FileStream(@"Content/SaveFiles/GameTestSave3.xml", FileMode.Create))
+            //{
+            //    serializer.Serialize(tw, mySave);
+            //    tw.Close();
+            //}
+            
+            //ORDER REALLY MATTERS
+            FileStream fileStream = File.OpenWrite(@"Content/SaveFiles/BinarySave.txt");
+            BinaryWriter binaryWriter = new BinaryWriter(fileStream);
+
+            GameSerializer.SavePlayer(Game1.Player, binaryWriter, 1);
+
+            binaryWriter.Flush();
+            binaryWriter.Close();
         }
 
         public void Load()
         {
-            //Load XML file with all pre-made items into itemVault.
-            //Game1.ItemVault.RawItems.Load(@"Content/StartUpData/itemData.xml");
 
-            XmlSerializer deSerializer = new XmlSerializer(typeof(SaveData));
+            FileStream fileStream = File.OpenRead(@"Content/SaveFiles/BinarySave.txt");
+            BinaryReader binaryReader = new BinaryReader(fileStream);
+            GameSerializer.LoadPlayer(Game1.Player, binaryReader, 1);
 
-            StreamReader reader = new StreamReader(@"Content/SaveFiles/GameTestSave3.xml");
+            binaryReader.Close();
+
+
+            ////Load XML file with all pre-made items into itemVault.
+            ////Game1.ItemVault.RawItems.Load(@"Content/StartUpData/itemData.xml");
+
+            //XmlSerializer deSerializer = new XmlSerializer(typeof(SaveData));
+
+            //StreamReader reader = new StreamReader(@"Content/SaveFiles/GameTestSave3.xml");
             
-                mySave = (SaveData)deSerializer.Deserialize(reader);
-            AssignLoad();
-                reader.Close();
+            //    mySave = (SaveData)deSerializer.Deserialize(reader);
+            //AssignLoad();
+            //    reader.Close();
 
         }
 
--- SecretProject/SecretProject/Class/Universal/GameSerializer.cs
+++ SecretProject/SecretProject/Class/Universal/GameSerializer.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SecretProject.Class.Playable;
+using Microsoft.Xna.Framework;
+using SecretProject.Class.ItemStuff;
 
 namespace SecretProject.Class.Universal
 {
@@ -12,13 +14,90 @@ namespace SecretProject.Class.Universal
     {
 
 
-
+        //order really really matters
         public static void SavePlayer(Player player, BinaryWriter writer, float version)
         {
             writer.Write(Game1.Player.Position.X);
             writer.Write(Game1.Player.Position.Y);
             writer.Write(Game1.Player.Name);
-            //writer.Write(Game1.)
+            WriteInventory(Game1.Player.Inventory, writer, version);
+            
+
+            
+        }
+
+        public static void LoadPlayer(Player player, BinaryReader reader, float version)
+        {
+            player.Position = new Vector2(reader.ReadSingle(), reader.ReadSingle());
+            player.Name = reader.ReadString();
+            
+        }
+
+        public static void WriteInventory(Inventory inventory, BinaryWriter writer, float version)
+        {
+            writer.Write(inventory.Capacity);
+
+            for(int i=0; i< inventory.Capacity; i++)
+            {
+                if (inventory.currentInventory[i].SlotItems.Count > 0)
+                {
+                    for (int j = 0; j < inventory.currentInventory[i].SlotItems.Count; j++)
+                    {
+                        WriteItem(inventory.currentInventory[i].SlotItems[0], writer, version);
+                    }
+                }   
+            } 
+        }
+
+        public static void LoadInventory(Inventory inventory, BinaryReader reader, float version)
+        {
+            //read capacity
+            int capacity = reader.ReadInt32();
+            Inventory newInventory = new Inventory(capacity);
+            for(int i =0; i< capacity; i++)
+            {
+                //need to figure out how to find each slot's capacity
+                for(int j=0; j < 5; j++)
+                {
+                    newInventory.currentInventory[i].AddItemToSlot(LoadItem(reader, version));
+                }
+                newInventory.currentInventory
+                 
+            }
+
+
+        }
+
+        public static void WriteItem(Item item, BinaryWriter writer, float version)
+        {
+            writer.Write(item.Name);
+            writer.Write(item.ID);
+            writer.Write(item.Count);
+            writer.Write(item.InvMaximum);
+            writer.Write(item.WorldMaximum);
+            writer.Write(item.Ignored);
+            writer.Write(item.IsDropped);
+            writer.Write(item.IsPlaceable);
+            writer.Write(item.id);
+            writer.Write(item.TextureString);
+            writer.Write(item.Price);
+        }
+
+        public static Item LoadItem( BinaryReader reader, float version)
+        {
+            Item item = new Item();
+            item.Name = reader.ReadString();
+            item.ID = reader.ReadInt32();
+            item.Count = reader.ReadInt32();
+            item.InvMaximum = reader.ReadInt32();
+            item.WorldMaximum = reader.ReadInt32();
+            item.Ignored = reader.ReadBoolean();
+            item.IsDropped = reader.ReadBoolean();
+            item.IsPlaceable = reader.ReadBoolean();
+            item.TextureString = reader.ReadString();
+            item.Price = reader.ReadInt32();
+
+            return item;
         }
 
 
