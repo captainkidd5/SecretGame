3bff184da600f76bd15c7cad40724540971eb217
unknown <thiggin6@lion.lmu.edu>
2019-09-02 11:40:14 -0700

    added graph class

---
+++ SecretProject/SecretProject/Class/PathFinding/Graph.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecretProject.Class.PathFinding
+{
+    public class Graph
+    {
+        private List<int>[] childNodes;
+        public int Size { get { return this.childNodes.Length; } }
+        public Graph(int size)
+        {
+            this.childNodes = new List<int>[size];
+            for(int i =0; i < size; i++)
+            {
+                this.childNodes[i] = new List<int>();
+            }
+        }
+
+        public Graph(List<int>[] childNodes)
+        {
+            this.childNodes = childNodes;
+        }
+
+        public void AddEdge(int baseNode, int nodeToConnect)
+        {
+            childNodes[baseNode].Add(nodeToConnect);
+        }
+
+        public void RemoveEdge(int baseNode, int nodeToConnect)
+        {
+            childNodes[baseNode].Remove(nodeToConnect);
+        }
+
+        public bool HasEdge(int baseNode, int nodeToConnect)
+        {
+            bool hasEdge = childNodes[baseNode].Contains(nodeToConnect);
+            return hasEdge;
+        }
+
+        public IList<int> GetSuccessors(int v)
+
+        {
+            return childNodes[v];
+        }
+
+    }
+}
