06febaebad917cb4971a5e7c7403f241d2177ca2
Tucker Higgins <thiggin6@lion.lmu.edu>
2019-11-14 13:47:16 -0800

    added new pathfinding code, saving before trying to implement it

---
+++ SecretProject/SecretProject/Class/PathFinding/PathFinder/AuthorAttribute.cs
@@ -0,0 +1,30 @@
+﻿//
+//  THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+//  KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+//  IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR
+//  PURPOSE. IT CAN BE DISTRIBUTED FREE OF CHARGE AS LONG AS THIS HEADER 
+//  REMAINS UNCHANGED.
+//
+//  Email:  gustavo_franco@hotmail.com
+//
+//  Copyright (C) 2006 Franco, Gustavo 
+//
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecretProject.Class.PathFinding.PathFinder
+{
+    internal class AuthorAttribute : Attribute
+    {
+        #region Constructors
+        public AuthorAttribute(string authorName)
+        {
+        }
+        #endregion
+    }
+}
---
+++ SecretProject/SecretProject/Class/PathFinding/PathFinder/IPathFinder.cs
@@ -0,0 +1,105 @@
+﻿//  IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR
+//  PURPOSE. IT CAN BE DISTRIBUTED FREE OF CHARGE AS LONG AS THIS HEADER 
+//  REMAINS UNCHANGED.
+//
+//  Email:  gustavo_franco@hotmail.com
+//
+//  Copyright (C) 2006 Franco, Gustavo 
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace SecretProject.Class.PathFinding.PathFinder
+{
+    [Author("Franco, Gustavo")]
+    interface IPathFinder
+    {
+        #region Events
+        event PathFinderDebugHandler PathFinderDebug;
+        #endregion
+
+        #region Properties
+        bool Stopped
+        {
+            get;
+        }
+
+        HeuristicFormula Formula
+        {
+            get;
+            set;
+        }
+
+        bool Diagonals
+        {
+            get;
+            set;
+        }
+
+        bool HeavyDiagonals
+        {
+            get;
+            set;
+        }
+
+        int HeuristicEstimate
+        {
+            get;
+            set;
+        }
+
+        bool PunishChangeDirection
+        {
+            get;
+            set;
+        }
+
+        bool ReopenCloseNodes
+        {
+            get;
+            set;
+        }
+
+        bool TieBreaker
+        {
+            get;
+            set;
+        }
+
+        int SearchLimit
+        {
+            get;
+            set;
+        }
+
+        double CompletedTime
+        {
+            get;
+            set;
+        }
+
+        bool DebugProgress
+        {
+            get;
+            set;
+        }
+
+        bool DebugFoundPath
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Methods
+        void FindPathStop();
+        List<PathFinderNode> FindPath(Point start, Point end);
+        #endregion
+
+    }
+}
