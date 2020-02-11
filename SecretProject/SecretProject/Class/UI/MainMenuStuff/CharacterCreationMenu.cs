acfa9a45c9d0c5ab030e580c9d30f2c10f994f3e
Tucker Higgins <thiggin6@lion.lmu.edu>
2020-02-11 15:13:52 -0800

    working on character creation menu

---
+++ SecretProject/SecretProject/Class/UI/MainMenuStuff/CharacterCreationMenu.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecretProject.Class.UI.MainMenuStuff
+{
+    public class CharacterCreationMenu
+    {
+        public CharacterCreationMenu()
+        {
+
+        }
+        public void Update(GameTime gameTime)
+        {
+
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+}
