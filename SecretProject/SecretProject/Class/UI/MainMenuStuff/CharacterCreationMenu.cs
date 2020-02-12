1517136ed936bea503578e9be765a3b840ec5965
Tucker Higgins <thiggin6@lion.lmu.edu>
2020-02-11 16:07:49 -0800

    working on character creation menu

--- SecretProject/SecretProject/Class/UI/MainMenuStuff/CharacterCreationMenu.cs
+++ SecretProject/SecretProject/Class/UI/MainMenuStuff/CharacterCreationMenu.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using SecretProject.Class.MenuStuff;
+using SecretProject.Class.Universal;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,29 +12,39 @@ namespace SecretProject.Class.UI.MainMenuStuff
 {
     public class CharacterCreationMenu
     {
+        public string StartButtonString { get; set; }
+        public float Scale { get; set; }
         public GraphicsDevice Graphics { get; set; }
         public Vector2 Position { get; set; }
         public Rectangle BackGroundSourceRectangle { get; set; }
         public Rectangle CharacterPortraitWindow { get; set; }
         public TypingWindow TypingWindow { get; set; }
+
+        public Button StartNewGameButton { get; set; }
         public CharacterCreationMenu(GraphicsDevice graphics, Vector2 position)
         {
+            this.StartButtonString = "GO!";
+            this.Scale = 3f;
             this.Graphics = graphics;
             this.Position = position;
             this.BackGroundSourceRectangle = new Rectangle(832, 496, 192, 160);
             this.CharacterPortraitWindow = new Rectangle(896, 432, 64, 64);
             this.TypingWindow = new TypingWindow(graphics, position);
+
+            this.StartNewGameButton = new Button(Game1.AllTextures.UserInterfaceTileSet, new Rectangle(896, 656, 64,22), this.Graphics, new Vector2(this.Position.X + this.BackGroundSourceRectangle.Width /2 * this.Scale - 64 / 2 * this.Scale, this.Position.Y + this.BackGroundSourceRectangle.Height *this.Scale - 22 /2 * this.Scale + 32), Controls.CursorType.Normal, 3f, null);
         }
         public void Update(GameTime gameTime)
         {
+            this.StartNewGameButton.Update(Game1.myMouseManager);
             this.TypingWindow.Update(gameTime);
         }
         public void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch.Draw(Game1.AllTextures.UserInterfaceTileSet, this.Position, this.BackGroundSourceRectangle, Color.White, 0f, Game1.Utility.Origin, 3f, SpriteEffects.None, Game1.Utility.StandardTextDepth - .04f);
-            spriteBatch.Draw(Game1.AllTextures.UserInterfaceTileSet,new Vector2(this.Position.X + this.BackGroundSourceRectangle.Width /2 - this.CharacterPortraitWindow.Width,
-                this.Position.Y + this.BackGroundSourceRectangle.Height/ 2 - this.CharacterPortraitWindow.Height),this.CharacterPortraitWindow, Color.White, 0f, Game1.Utility.Origin, 3f, SpriteEffects.None, Game1.Utility.StandardTextDepth);
+            spriteBatch.Draw(Game1.AllTextures.UserInterfaceTileSet,new Vector2(this.Position.X + this.BackGroundSourceRectangle.Width /2 * this.Scale - this.CharacterPortraitWindow.Width /2 * Scale,
+                this.Position.Y + this.BackGroundSourceRectangle.Height/ 2 * this.Scale - this.CharacterPortraitWindow.Height / 2 * Scale),this.CharacterPortraitWindow, Color.White, 0f, Game1.Utility.Origin, 3f, SpriteEffects.None, Game1.Utility.StandardTextDepth);
             this.TypingWindow.Draw(spriteBatch);
+            this.StartNewGameButton.Draw(spriteBatch, Game1.AllTextures.MenuText, this.StartButtonString, StartNewGameButton.Position, StartNewGameButton.Color, Utility.StandardButtonDepth, Game1.Utility.StandardTextDepth, this.Scale);
         }
     }
 }
