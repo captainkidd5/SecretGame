0c295cbf19c22ba537d4d920e1e5bd8f4bc99ff7
Tucker Higgins <thiggin6@lion.lmu.edu>
2020-02-19 15:14:18 -0800

    can now change hairstyles in game

--- SecretProject/SecretProject/Class/Playable/Wardrobe.cs
+++ SecretProject/SecretProject/Class/Playable/Wardrobe.cs
@@ -19,9 +19,15 @@ namespace SecretProject.Class.Playable
         Shoes = 3,
         Base = 4
     }
-    
+
     public class Wardrobe : ISaveable
     {
+        public GraphicsDevice Graphics { get; set; }
+
+        public int MaxSpriteIndex { get; set; }
+
+
+
         public int HairIndex { get; set; }
         public int ShirtIndex { get; set; }
         public int PantsIndex { get; set; }
@@ -36,6 +42,10 @@ namespace SecretProject.Class.Playable
 
         public Wardrobe(GraphicsDevice graphics, Vector2 playerPosition)
         {
+            this.Graphics = graphics;
+            //Height of player part spritesheet. All should be the same. Each part has a width of 16 and a height of 32 so 64 would mean two different clothing options!
+            this.MaxSpriteIndex = 64;
+
             this.BasicMovementAnimations = new Sprite[4, 5];
             BasicMovementAnimations[(int)Dir.Down, (int)ClothingLayer.Hair] = new Sprite(graphics, Game1.AllTextures.PlayerHair, 0, 0, 16, 32, 6, .1f, playerPosition) { LayerDepth = .00000011f };//, Color = Color.Black };
             BasicMovementAnimations[(int)Dir.Down, (int)ClothingLayer.Shirt] = new Sprite(graphics, Game1.AllTextures.PlayerShirt, 0, 32, 16, 32, 6, .1f, playerPosition) { LayerDepth = .00000010f };
@@ -73,12 +83,41 @@ namespace SecretProject.Class.Playable
             {
                 for (int j = 0; j < BasicMovementAnimations.GetLength(1); j++)
                 {
-                    if(setFrameZero)
+                    if (setFrameZero)
                     {
                         BasicMovementAnimations[i, j].SetFrame(0);
                     }
-                   
+
                     BasicMovementAnimations[i, j].UpdateAnimationPosition(position);
+                }
+            }
+        }
+
+        public void CycleClothing(ClothingLayer layer, Vector2 playerPosition, bool backwards = false)
+        {
+            int frameChangeAmt = 32;
+            if (backwards)
+            {
+                frameChangeAmt = frameChangeAmt * -1; 
+            }
+            int newSpriteY = BasicMovementAnimations[0, (int)layer].FirstFrameY + frameChangeAmt;
+
+            if (newSpriteY > this.MaxSpriteIndex)
+            {
+                newSpriteY = 0;
+            }
+            if (newSpriteY < 0)
+            {
+                newSpriteY = this.MaxSpriteIndex;
+            }
+
+            float oldLayerDepth = BasicMovementAnimations[0, (int)layer].LayerDepth;
+
+            for (int i = 0; i < BasicMovementAnimations.GetLength(0); i++)
+            {
+                for (int j = 0; j < BasicMovementAnimations.GetLength(1); j++)
+                {
+                    BasicMovementAnimations[i, (int)layer].FirstFrameY = newSpriteY;
                 }
             }
         }
