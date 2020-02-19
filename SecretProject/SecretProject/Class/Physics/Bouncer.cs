196d34e7c8da5de9797b942afea87be703c4e572
Tucker Higgins <thiggin6@lion.lmu.edu>
2020-02-19 10:06:14 -0800

    working a bit more on item bouncing

--- SecretProject/SecretProject/Class/Physics/Bouncer.cs
+++ SecretProject/SecretProject/Class/Physics/Bouncer.cs
@@ -20,7 +20,8 @@ namespace SecretProject.Class.Physics
 
         public Vector2 AirResistance { get; set; } = new Vector2(2f, 12f);
         public bool IsActive { get; set; }
- 
+
+        public int NumBounces { get; set; }
 
         public Bouncer(Vector2 bounceObjectStartPosition, Dir directionToBounce)
         {
@@ -28,10 +29,16 @@ namespace SecretProject.Class.Physics
             this.DirectionToBounce = directionToBounce;
 
             this.VerticalFloor = this.VerticalSpeed * Game1.Utility.RFloat(.75f, 3f);
+
+            int horizontalMultiplier = 1;
+            if(Game1.Utility.RNumber(0,3) == 1)
+            {
+                horizontalMultiplier = -1;
+            }
             switch(DirectionToBounce)
             {
                 case Dir.Down:
-                    Velocity = new Vector2(HorizontalSpeed * Game1.Utility.RNumber(-1, 2), -this.VerticalSpeed);
+                    Velocity = new Vector2(HorizontalSpeed * horizontalMultiplier, -this.VerticalSpeed);
                     break;
                 case Dir.Left:
                     Velocity = new Vector2(HorizontalSpeed, -this.VerticalSpeed);
@@ -40,16 +47,16 @@ namespace SecretProject.Class.Physics
                     Velocity = new Vector2(-HorizontalSpeed, -this.VerticalSpeed);
                     break;
                 case Dir.Up:
-                    Velocity = new Vector2(HorizontalSpeed * Game1.Utility.RNumber(-1, 2), -this.VerticalSpeed);
+                    Velocity = new Vector2(HorizontalSpeed * horizontalMultiplier, -this.VerticalSpeed);
                     break;
                 default:
                     Velocity = new Vector2(HorizontalSpeed, - this.VerticalSpeed);
                     break;
             }
             this.BaseVelocity = Velocity;
-            this.VerticalFloor = bounceObjectStartPosition.Y + Game1.Utility.RNumber(3,8);
+            this.VerticalFloor = bounceObjectStartPosition.Y + Game1.Utility.RNumber(-3,8);
             this.IsActive = true;
-
+            this.NumBounces = 0;
         }
 
         public Vector2 Update(GameTime gameTime)
@@ -59,13 +66,18 @@ namespace SecretProject.Class.Physics
             this.BounceObjectPosition += Velocity;
             if (this.BounceObjectPosition.Y >= this.VerticalFloor)
             {
-                this.Velocity = this.BaseVelocity *.8f;
+                
+                float newVelocityY = this.BaseVelocity.Y * .9f;
+                float newVelocityX = this.BaseVelocity.X * .6f;
+                this.Velocity = new Vector2(newVelocityX, newVelocityY);
                 this.BaseVelocity = this.Velocity;
 
-                
+                NumBounces++;
+
+
             }
 
-            if (Math.Abs(Velocity.X) <= .0004 || Math.Abs(Velocity.Y) <= .005)
+            if (NumBounces > 6||Math.Abs(Velocity.X) <= .01 || Math.Abs(Velocity.Y) <= .000005)
             {
                 this.IsActive = false;
             }
