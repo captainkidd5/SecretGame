345ec1d2ae54982b3128e8a0bead718338b3df85
Tucker Higgins <thiggin6@lion.lmu.edu>
2020-03-22 05:46:59 -1000

    save before working on wardrobe

---
+++ SecretProject/SecretProject/Class/Playable/ClothingSet.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecretProject.Class.Playable
+{
+    public enum ClothingType
+    {
+        Hair = 1,
+        Shirt = 2,
+        Pants = 3,
+        Shoes = 4,
+        Base = 5
+    }
+    public class ClothingSet
+    {
+        public GraphicsDevice Graphics { get; set; }
+        public ClothingType ClothingType { get; set; }
+        public List<ClothingPiece> Pieces { get; set; }
+
+        public ClothingSet(GraphicsDevice graphics, ClothingType clothingType)
+        {
+            this.Graphics = graphics;
+            this.ClothingType = clothingType;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+}
