769ebf7e619b7c86c47cd13437673ee6baaa1580
Tucker Higgins <thiggin6@lion.lmu.edu>
2020-07-05 09:38:15 -0700

    working on navigator class

--- SecretProject/SecretProject/Class/Misc/GrassCreature.cs
+++ SecretProject/SecretProject/Class/Misc/GrassCreature.cs
@@ -5,12 +5,21 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using SecretProject.Class.PathFinding;
+using SecretProject.Class.PathFinding.PathFinder;
 using SecretProject.Class.SpriteFolder;
 
 namespace SecretProject.Class.Misc
 {
+    public enum GrassCreatureType
+    {
+       none = 0,
+       mouse = 1
+    }
     public class GrassCreature : FunItems
     {
+        public GrassCreatureType GrassCreatureType { get; set; }
+        public string EntityName { get; set; }
         public List<FunItems> FunItems { get; set; }
 
         private Texture2D Texture { get; set; }
@@ -21,14 +30,33 @@ namespace SecretProject.Class.Misc
 
         private float Speed { get; set; } = 1.7f;
 
+        private Navigator Navigator { get; set; }
 
-        public GrassCreature(GraphicsDevice graphics)
+
+        public GrassCreature(GrassCreatureType grassCreatureType, GraphicsDevice graphics, Vector2 position, List<FunItems> funItems)
         {
+            this.Position = position;
+            this.FunItems = funItems;
+            this.Texture = Game1.AllTextures.ButterFlys;
+            
             this.AnimatedSprite = new Sprite[3];
+            this.EntityName = GetName(funItems.Count);
 
-            this.AnimatedSprite[0] = new Sprite(graphics, Texture, 0, 64, 16, 16, 3, .25f, this.Position, changeFrames: false);
-            this.AnimatedSprite[1] = new Sprite(graphics, Texture, 16, 64, 16, 16, 3, .25f, this.Position, changeFrames: false);
-            this.AnimatedSprite[2] = new Sprite(graphics, Texture, 32, 64, 16, 16, 3, .25f, this.Position, changeFrames: false);
+            switch(grassCreatureType)
+            {
+                case GrassCreatureType.mouse:
+                    this.AnimatedSprite[0] = new Sprite(graphics, Texture, 0, 64, 16, 16, 3, .25f, this.Position, changeFrames: false);
+                    this.AnimatedSprite[1] = new Sprite(graphics, Texture, 16, 64, 16, 16, 3, .25f, this.Position, changeFrames: false);
+                    this.AnimatedSprite[2] = new Sprite(graphics, Texture, 32, 64, 16, 16, 3, .25f, this.Position, changeFrames: false);
+                    break;
+            }
+            
+            this.Navigator = new Navigator(this.EntityName, Game1.CurrentStage.AllTiles.PathGrid.Weight);
+        }
+
+        private string GetName(int index)
+        {
+            return this.GrassCreatureType.ToString() + "_" + index.ToString();
         }
 
         public void Update(GameTime gameTime)
---
+++ SecretProject/SecretProject/Class/PathFinding/Navigator.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xna.Framework;
+using SecretProject.Class.PathFinding.PathFinder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecretProject.Class.PathFinding
+{
+    public class Navigator
+    {
+        public string EntityName { get; set; }
+        public PathFinderFast PathFinderFast { get; set; }
+        private List<PathFinderNode> CurrentPath { get; set; }
+        private byte[,] Grid { get; set; }
+
+
+
+        public Navigator(string entityName, byte[,] grid)
+        {
+            this.EntityName = entityName;
+            this.Grid = grid;
+            this.PathFinderFast = new PathFinderFast(Grid);
+        }
+        public bool TryFindNewPath( Vector2 currentEntityPosition, Point endPoint)
+        {
+            PathFinderFast finder = new PathFinderFast(this.Grid);
+
+
+            Point start = new Point(Math.Abs((int)currentEntityPosition.X / 16),
+             (Math.Abs((int)currentEntityPosition.Y / 16)));
+
+            this.CurrentPath = finder.FindPath(start, endPoint, this.EntityName);
+            if (this.CurrentPath == null)
+            {
+                this.CurrentPath = new List<PathFinderNode>();
+                return false;
+                throw new Exception(this.EntityName + " was unable to find a path between " + start + " and " + endPoint);
+            }
+            return true;
+        }
+    }
+}
